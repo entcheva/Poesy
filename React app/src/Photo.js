eb3fc8189295e5be64de7fecbbe948cd0e12193c
entcheva <christina.entcheva@gmail.com>
2017-02-11 11:04:34 -0500

    Render basic view in React

---
+++ React app/src/Photo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Photo extends Component {
+  render() {
+    return (
+
+      <div>
+        <h2>Hello from the Photo component</h2>
+        <div>
+          <img src="https://media.giphy.com/media/EdnHfe5SWGTm/giphy.gif"/>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ React app/src/Poem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Poem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello from the Poem component</h2>
+      </div>
+    )
+  }
+}
