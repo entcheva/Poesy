22ae76daf6e6c1f69a17008afc594469ca4b85ee
deaundrapeek <jingyufanclub@gmail.com>
2017-02-11 11:36:46 -0500

    some seeds

--- feyonce-api/db/seeds.rb
+++ feyonce-api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ Postcard.create(poem: "hey, sup, yo girl", image_url:"https://media.giphy.com/media/3oz8xuBPs02ColcUQo/source.gif")
+
+ Postcard.create(poem: "i like my body when it is with your body. It is so quite new a thing. Muscles better and nerves more.", image_url:"https://media.giphy.com/media/vAHZ9rc8rY8zm/giphy.gif")
