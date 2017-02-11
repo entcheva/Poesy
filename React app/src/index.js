0efb87e7772d0d54ecc925283407744c0221068f
entcheva <christina.entcheva@gmail.com>
2017-02-11 11:51:46 -0500

    Add placeholder Axios API call

--- React app/src/index.js
+++ React app/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import App from './App'
+import './index.css'
+
+getPoemAPI => {
+  const poem = axios.get('/user?ID=12345') // placeholder URL
+  .then( response => response.data) // what should we do with the response?
+}
+
+getPhotoAPI => {
+  const photo = axios.get('/user?ID=12345') // placeholder URL
+  .then( response => const photoResponse = photo) // what should we do with the response?
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
